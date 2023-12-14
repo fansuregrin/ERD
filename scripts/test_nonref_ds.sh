90368563175c7f21930129f171d7942b8b71833e
fansuregrin <pwz113436@gmail.com>
2023-12-14 20:31:16 +0800

    update test_ref_ds and test_nonref_ds scripts

--- scripts/test_nonref_ds.sh
+++ scripts/test_nonref_ds.sh
@@ -1,6 +1,7 @@
 GREEN="\e[32m"
 RED="\e[31m"
 BOLD="\e[1m"
+BOLD_GREEN="\e[1;32m"
 ENDSTYLE="\e[0m"
 
 declare -A ds_dict
@@ -9,20 +10,22 @@ ds_dict=([U45]="configs/dataset/u45.yaml"
          [UPoor200]="configs/dataset/upoor200.yaml"
          [UW2023]="configs/dataset/uw2023.yaml")
 
-if [ $# -lt 4 ]
+if [ $# -lt 5 ]
 then
     echo -e "${RED}PLEASE PASS IN THE FOLLOWING ARGUMENTS IN ORDER!${ENDSTYLE}"
     echo -e "1) model_v"
     echo -e "2) net_cfg"
     echo -e "3) name"
     echo -e "4) epoch"
-    echo -e "for example: ${BOLD}bash ${0} ie configs/network/erd_15blocks_2down.yaml LSUI_01 299${ENDSTYLE}"
+    echo -e "5) load_prefix"
+    echo -e "for example: \"${BOLD_GREEN}bash ${0} ie configs/network/ra_9blocks_2down.yaml LSUI_01 299 weights${ENDSTYLE}\""
     exit -1
 fi
 model_v=${1}
 net_cfg=${2}
 name=${3}
 epoch=${4}
+load_prefix=${5}
 
 for ds_name in ${!ds_dict[@]};
 do
@@ -31,5 +34,6 @@ do
     --net_cfg ${net_cfg} \
     --name ${name} \
     --test_name ${ds_name} \
-    --epoch ${epoch}
+    --epoch ${epoch} \
+    --load_prefix ${load_prefix}
 done
--- scripts/test_ref_ds.sh
+++ scripts/test_ref_ds.sh
@@ -1,6 +1,7 @@
 GREEN="\e[32m"
 RED="\e[31m"
 BOLD="\e[1m"
+BOLD_GREEN="\e[1;32m"
 ENDSTYLE="\e[0m"
 
 declare -A ds_dict
@@ -10,20 +11,22 @@ ds_dict=([LSUI]="configs/dataset/lsui.yaml"
          [EUVP515]="configs/dataset/euvp515.yaml")
 
 
-if [ $# -lt 4 ]
+if [ $# -lt 5 ]
 then
     echo -e "${RED}PLEASE PASS IN THE FOLLOWING ARGUMENTS IN ORDER!${ENDSTYLE}"
     echo -e "1) model_v"
     echo -e "2) net_cfg"
     echo -e "3) name"
     echo -e "4) epoch"
-    echo -e "for example: ${BOLD}bash ${0} ie configs/network/ra_15blocks_2down.yaml LSUI_01 299${ENDSTYLE}"
+    echo -e "5) load_prefix"
+    echo -e "for example: \"${BOLD_GREEN}bash ${0} ie configs/network/ra_9blocks_2down.yaml LSUI_01 299 weights${ENDSTYLE}\""
     exit -1
 fi
 model_v=${1}
 net_cfg=${2}
 name=${3}
 epoch=${4}
+load_prefix=${5}
 
 for ds_name in ${!ds_dict[@]};
 do
@@ -32,5 +35,6 @@ do
     --net_cfg ${net_cfg} \
     --name ${name} \
     --test_name ${ds_name} \
-    --epoch ${epoch}
+    --epoch ${epoch} \
+    --load_prefix ${load_prefix}
 done
